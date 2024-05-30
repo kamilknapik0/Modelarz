261a296fee40f99ad27fb1069c04978d55799667
Kamil <knapikkamil2002@gmail.com>
2024-05-30 15:37:59 +0200

    Dodanie nowych elementów UI i zmiana właściwości okna głównego

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -28,20 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelMenu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(51, 553);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "zakoncz";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelMenu.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelMenu.Controls.Add(this.btnExit);
+            this.panelMenu.Location = new System.Drawing.Point(-1, 0);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(224, 613);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // panelTop
+            // 
+            this.panelTop.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelTop.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panelTop.Location = new System.Drawing.Point(171, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(713, 134);
+            this.panelTop.TabIndex = 2;
+            // 
+            // panelMain
+            // 
+            this.panelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelMain.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panelMain.Location = new System.Drawing.Point(171, 106);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(713, 507);
+            this.panelMain.TabIndex = 3;
             // 
             // MainWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(650, 498);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(884, 611);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.panelMenu);
             this.Name = "MainWindow";
             this.Text = "Modelarz";
+            this.panelMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Panel panelMain;
     }
 }
 
--- MainWindow.cs
+++ MainWindow.cs
@@ -16,5 +16,10 @@ namespace Modelarz
         {
             InitializeComponent();
         }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
