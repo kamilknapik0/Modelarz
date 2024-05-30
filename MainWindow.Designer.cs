d6d236ac8f3cb1d6bf1b12489edce887bc19ca52
Jan Wąs <jwas@o365.us.edu.pl>
2024-05-30 17:12:21 +0200

    #

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -33,6 +33,8 @@
             this.panelTop = new System.Windows.Forms.Panel();
             this.panelMain = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.panelMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -42,6 +44,8 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panelMenu.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelMenu.Controls.Add(this.button2);
+            this.panelMenu.Controls.Add(this.label1);
             this.panelMenu.Controls.Add(this.button1);
             this.panelMenu.Controls.Add(this.btnExit);
             this.panelMenu.Location = new System.Drawing.Point(-1, 0);
@@ -85,13 +89,33 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(22, 53);
+            this.button1.Location = new System.Drawing.Point(13, 32);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(153, 44);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Menu1";
+            this.button1.Text = "Ekran główny";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MENU";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 44);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Katalog pacjentów";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // MainWindow
             // 
@@ -104,6 +128,7 @@
             this.Name = "MainWindow";
             this.Text = "Modelarz";
             this.panelMenu.ResumeLayout(false);
+            this.panelMenu.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -114,6 +139,8 @@
         private System.Windows.Forms.Panel panelMain;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
