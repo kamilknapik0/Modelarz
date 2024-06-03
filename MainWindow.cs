7f394334d93a8b413645892c77ebc08a5ab12d4a
Kamil <knapikkamil2002@gmail.com>
2024-06-03 19:23:29 +0200

    push

--- MainWindow.cs
+++ MainWindow.cs
@@ -55,81 +55,17 @@ namespace Modelarz
 
         private void buttonExport_Click(object sender, EventArgs e)
         {
-
-            // Your connection string
-            string connectionString = "User Id=msbd4;Password=haslo2024;Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=155.158.112.45)(PORT=1521)))(CONNECT_DATA=(SID=oltpstud)))";
-
-            // Your SQL query
-            string sqlQuery = "select p.pacjent_id, p.imie, p.nazwisko, p.pesel, p.data_urodzenia, m.nr_modelu, m.data_wykonania from pacjenci p join modeleortodontyczne m on p.pacjent_id = m.pacjent_id";
-
-            // Get data from database
-            Export export = new Export();
-            DataTable dataTable = export.GetDataFromDatabase(connectionString, sqlQuery);
-
-            // Define the file path for the Excel file
-            string filePath = "D:\\plik.xlsx";
-
-            // Export to Excel
-            export.ExportToExcel(dataTable, filePath);
-
-            // Inform the user
-            MessageBox.Show("Data exported to Excel successfully!");
-
-
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.Filter = "Excel Files|.xls;.xlsx";
-                saveFileDialog.Title = "Save an Excel File";
-                saveFileDialog.ShowDialog();
-
-                if (saveFileDialog.FileName != "")
-                {
-                    ExportToExcel(dataTable, filePath);
-                }
-            
+            Main();
 
         }
-
-        public DataTable GetDataFromDatabase(string connectionString, string query)
-        {
-            DataTable dataTable = new DataTable();
-
-            using (OracleConnection connection = new OracleConnection(connectionString))
-            {
-                connection.Open();
-                using (OracleCommand command = new OracleCommand(query, connection))
-                {
-                    using (OracleDataAdapter adapter = new OracleDataAdapter(command))
-                    {
-                        adapter.Fill(dataTable);
-                    }
-                }
-            }
-            return dataTable;
-        }
-
-        public void ExportToExcel(DataTable dataTable, string filePath)
-        {
-            OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
-
-            using (var package = new ExcelPackage())
-            {
-                var worksheet = package.Workbook.Worksheets.Add("Sheet1");
-
-                // Load the data table into the worksheet, starting at cell A1
-                worksheet.Cells["A1"].LoadFromDataTable(dataTable, true);
-
-                // Save the package to the specified file
-                package.SaveAs(new FileInfo(filePath));
-            }
-        }
-
+        
 
         void closeForms()
         {
             List<Form> formsToClose = new List<Form>();
             foreach (Form frm in Application.OpenForms)
             {
-                if (frm is Home || frm is Katalog || frm is Edycja || frm is Export)
+                if (frm is Home || frm is Katalog || frm is Edycja)
                 {
                     formsToClose.Add(frm);
                 }
@@ -177,18 +113,72 @@ namespace Modelarz
             frm.Show();
         }
 
-        
-
-        void openExport()
+        public void Main()
         {
-            Export frm = new Export()
+            // Your connection string
+            string connectionString = "User Id=msbd4;Password=haslo2024;Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=155.158.112.45)(PORT=1521)))(CONNECT_DATA=(SID=oltpstud)))";
+
+            // Your SQL query
+            string sqlQuery = "select p.pacjent_id, p.imie, p.nazwisko, p.pesel, p.data_urodzenia, m.nr_modelu, m.data_wykonania from pacjenci p join modeleortodontyczne m on p.pacjent_id = m.pacjent_id";
+
+            // Get data from database
+            
+            DataTable dataTable = GetDataFromDatabase(connectionString, sqlQuery);
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx";
+            saveFileDialog.Title = "Save as Excel File";
+            saveFileDialog.FileName = "plik.xlsx";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
             {
-                TopLevel = false,
-                Dock = DockStyle.Fill
-            };
-            frm.FormBorderStyle = FormBorderStyle.None;
-            this.panelMain.Controls.Add(frm);
-            frm.Show();
+                // Export to Excel
+                ExportToExcel(dataTable, saveFileDialog.FileName);
+
+                // Inform the user
+                MessageBox.Show("Dane wyeksportowane pomyslnie!");
+            }
+
+            // Define the file path for the Excel file
+            string filePath = "D:\\plik.xlsx";
+
+            // Export to Excel
+            ExportToExcel(dataTable, filePath);
+
+           
+        }
+
+        public DataTable GetDataFromDatabase(string connectionString, string query)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                connection.Open();
+                using (OracleCommand command = new OracleCommand(query, connection))
+                {
+                    using (OracleDataAdapter adapter = new OracleDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            return dataTable;
+        }
+
+        public void ExportToExcel(DataTable dataTable, string filePath)
+        {
+            OfficeOpenXml.ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+
+                // Load the data table into the worksheet, starting at cell A1
+                worksheet.Cells["A1"].LoadFromDataTable(dataTable, true);
+
+                // Save the package to the specified file
+                package.SaveAs(new FileInfo(filePath));
+            }
         }
     }
 }
